7652d54a95035f70e6a1f3bb0d8f6baae6626f0b
rizqullahfakhri <rizqullahfakhri23@gmail.com>
2022-03-01 18:19:58 +0700

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <RH_ASK.h>
+
+#define LED_rx 8
+RH_ASK rf_driver;
+
+void setup() {
+  start:
+  Serial.begin(9600);
+  Serial.println("TURN ON");
+  if(!rf_driver.init()){
+    Serial.println("Failed");
+    goto start;
+  }
+  pinMode(LED_rx,OUTPUT);
+}
+
+void loop() {
+  uint8_t buf=0;
+  uint8_t buflen = sizeof(buf);
+  if(rf_driver.recv(&buf,&buflen)){
+    Serial.println((byte)buf,BIN);
+  }
+  
+
+    // uint8_t buf[11];
+    // uint8_t buflen = sizeof(buf);
+    // // Check if received packet is correct size
+    // if (rf_driver.recv(buf, &buflen))
+    // {
+      
+    //   // Message received with valid checksum
+    //   Serial.print("Message Received: ");
+    //   Serial.println((char*)buf);         
+    // }
+}
